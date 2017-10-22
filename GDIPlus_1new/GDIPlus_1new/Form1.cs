a6f243ef3b3414dbf92aafc6946d50f57fb0bf14
Paweena27 <59030185@kmitl.ac.th>
2017-10-22 11:52:40 +0700

    W09P20

--- GDIPlus_1new/GDIPlus_1new/Form1.cs
+++ GDIPlus_1new/GDIPlus_1new/Form1.cs
@@ -19,6 +19,27 @@ namespace GDIPlus_1new
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            this.SetClientSizeCore(500, 600);
+            Graphics g = e.Graphics;
+            HatchBrush brush;
+            int x = 20;
+            int y = 20;
+
+            foreach (HatchStyle brushStyle in Enum.GetValues(typeof(System.Drawing.Drawing2D.HatchStyle)))
+            {
+                brush = new HatchBrush(brushStyle, Color.Navy, Color.Yellow);
+                g.FillRectangle(brush, x, y, 40, 20);
+                y += 30;
+                if ((y + 30) > this.ClientSize.Height)
+                {
+                    y = 20;
+                    x += 150;
+
+
+                }
+
+            }
+            g.Dispose();
 
         }
     }
